376c67b0808122f356dd453a6db60ed23e87ab25
Colin Sauze <cos@aber.ac.uk>
2013-05-03 00:52:26 +0100

    GPL message

--- email_processor/rendermap.sh
+++ email_processor/rendermap.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
+#An internet enabled TV photo frame for the Raspberry Pi
+#Copyright (C) 2013 Colin Sauze
+      
+#This program is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 3 of the License, or
+#(at your option) any later version.
+                        
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#You should have received a copy of the GNU General Public License
+#along with this program; if not, write to the Free Software Foundation,
+#Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+                                                                
+                                                                
+                                                                
 . ./config.sh
 scp -P $port $scphost:$checkin_path/checkin.* .
 
